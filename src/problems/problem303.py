4fc9194f336b31001f8292bd29d0fc7c88e93eb5
igor <igor27school@gmail.com>
2012-12-17 20:18:23 -0800

    New solution to problem 303

--- src/problems/problem303.py
+++ src/problems/problem303.py
@@ -4,56 +4,49 @@ Created on Dec 15, 2012
 @author: Igor
 '''
 
-import heapq
+MAX = 10000
+undiscovered_numbers = range(1, MAX+1)
+undiscovered_numbers.remove(9999)
+dict = {9999:1111333355557778}
 
-HIGHEST_ALLOWED = 2
+# The less numbers we have left, the less we iterate
+def crossout_numbers(number):
+    # Can't remove numbers while iterating, remember them
+    list_remove = []
+    for n in undiscovered_numbers:
+        if number % n == 0:
+            list_remove.append(n)
+            dict[n] = number / n
+    # Remove numbers from the list because we found their value
+    for n in list_remove:
+        # Also use f(10*n) = f(n)
+        counter = n
+        while counter in undiscovered_numbers:
+            undiscovered_numbers.remove(counter)
+            dict[counter] = dict[n]
+            counter *= 10
 
-# Check if all the digits are in the right range
-def sat_const(n):
-    for chr in str(n):
-        if int(chr) > HIGHEST_ALLOWED:
-            return False
-    return True
+def to_base(n, base):
+    answer = 0
+    mult = 1
+    while (n > 0):
+        answer += mult * (n % base)
+        n //= base
+        mult *= 10
+    return answer
 
-# Pre-loading values that are hard to find
-f_dict = {9:1358,
-          99:11335578,
-          999:111333555778,
-          9999:1111333355557778}
-
-# Finding the right multiplier starting from the last digit
-# keeping the list of endings that produce products with good endings
-def f(n):
-    if sat_const(n):
-        return 1
-    if n in f_dict:
-        return f_dict[n]
-    if n % 10 == 0:
-        print "Processing %d" % n
-        return f(n/10)
-    DIGITS = range(10);
-    numbers_to_try = range(1, 10)
-    good_mults = []
-    mod = 10
-    while True:
-        heapq.heapify(numbers_to_try)
-        while len(numbers_to_try) > 0:
-            number = heapq.heappop(numbers_to_try)
-            product_tail = (n * number) % mod
-            if sat_const(product_tail):
-                good_mults.append(number)
-                if sat_const(number * n):
-                    f_dict[n] = number
-                    return number
-        numbers_to_try = [mod * digit + number for digit in DIGITS for number in good_mults]
-        mod *= 10
+def fill_dict():
+    BASE = 3
+    i=1
+    while undiscovered_numbers and i < 3**16:
+        n = to_base(i, BASE)
+        crossout_numbers(n)
+        i += 1
 
 def run():
-    MAX = 10000
-    sum = 0
-    for i in xrange(1, MAX+1):
-        sum += f(i)
-    print "The sum is %d" % sum
+    fill_dict()
+    answer = sum(dict.values())
+    print "The sum is %d" % answer
 
 if __name__ == '__main__':
     import time
